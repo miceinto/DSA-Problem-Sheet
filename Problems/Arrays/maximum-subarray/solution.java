6f1e8b252f89d19e7f36cf80aa249651f7be7e75
Ritesh Kumar <7976ritesh@gmail.com>
2025-10-31 00:02:12 +0530

    added code

---
+++ Problems/Arrays/maximum-subarray/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxCurrent = nums[0], maxGlobal = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            maxCurrent = Math.max(nums[i], maxCurrent + nums[i]);
+            maxGlobal = Math.max(maxGlobal, maxCurrent);
+        }
+        return maxGlobal;
+    }
+}
---
+++ Problems/Arrays/maximum-subarray/solution.js
@@ -0,0 +1,8 @@
+function maxSubarray(nums) {
+    let maxCurrent = nums[0], maxGlobal = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        maxCurrent = Math.max(nums[i], maxCurrent + nums[i]);
+        maxGlobal = Math.max(maxGlobal, maxCurrent);
+    }
+    return maxGlobal;
+};
